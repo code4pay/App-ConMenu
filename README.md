6933ff591ae7be8e9094250b65ff843b8277f1e9
michaelm <michael@muellers.net.au>
2018-06-13 13:47:09 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::ConMenu - It's new $module
+
+# SYNOPSIS
+
+    use App::ConMenu;
+
+# DESCRIPTION
+
+App::ConMenu is ...
+
+# LICENSE
+
+Copyright (C) michaelm.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+michaelm <michael@muellers.net.au>
---
+++ lib/App/ConMenu.pm
@@ -0,0 +1,39 @@
+package App::ConMenu;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::ConMenu - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::ConMenu;
+
+=head1 DESCRIPTION
+
+App::ConMenu is ...
+
+=head1 LICENSE
+
+Copyright (C) michaelm.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+michaelm E<lt>michael@muellers.net.auE<gt>
+
+=cut
+
